ee6eed800cfdde4079eecd391562a51307e0ebd8
Z User <z@container>
2026-04-05 07:27:07 +0000

    fix: remove extra brace in promo codes migration designer

--- src/Kumashaurma.API/Migrations/20260405063717_AddPromoCodesAndOrderExtras.Designer.cs
+++ src/Kumashaurma.API/Migrations/20260405063717_AddPromoCodesAndOrderExtras.Designer.cs
@@ -17,7 +17,6 @@ namespace Kumashaurma.API.Migrations
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
-        {
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("ProductVersion", "9.0.0")
